7e67beb92fad64a19bd32346bfeb64ed36c148fa
Fairy7801 <104213056+Fairy7801@users.noreply.github.com>
2023-10-25 23:48:51 +0700

    Update layout

---
+++ app/src/main/java/com/example/petshop/activity/GiaoDichActivity.java
@@ -0,0 +1,16 @@
+package com.example.petshop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.petshop.R;
+
+public class GiaoDichActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giao_dich);
+    }
+}
